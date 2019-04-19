bae0911bb6bd6344140a3d24c46077cf95b0e558
文威 <749834002@qq.com>
2019-04-19 10:40:16 +0800

    根据目标位置查询附近的钥匙柜-最终版

--- src/main/java/com/aaa/project/system/api/ApiKeyController.java
+++ src/main/java/com/aaa/project/system/api/ApiKeyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 钥匙接口
@@ -21,12 +22,23 @@ public class ApiKeyController {
     @Autowired
     private IKeyContainerService keyContainerService;
 
+    /**
+     * 解析地址获取周围钥匙柜
+     *
+     * @param posLng
+     * @param posLat
+     * @return 钥匙柜集合
+     */
     @RequestMapping("/aroundContainer")
-    public AjaxResult aroundContainer(@RequestParam(name = "posLng",required = true) String posLng,
-                                      @RequestParam(name = "posLat",required = true) String posLat) {
+    public AjaxResult aroundContainer(@RequestParam(name = "posLng", required = true) String posLng,
+                                      @RequestParam(name = "posLat", required = true) String posLat) {
+        //1-新建结果对象
         AjaxResult ajaxResult = new AjaxResult();
-        List<KeyContainer> aroundContainerList = keyContainerService.getAroundContainer(posLng, posLat);
-        ajaxResult.put("aroundContainerList",aroundContainerList);
+        //2-根据经纬度得到周围钥匙柜
+        List<Map<String, Object>> aroundContainerList = keyContainerService.getAroundContainer(posLng, posLat);
+        //3-赋值
+        ajaxResult.put("aroundContainerList", aroundContainerList);
+        //4-返回结果
         return ajaxResult;
     }
 }
