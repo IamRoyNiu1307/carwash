43b46b49f4f61779dfad1fe0a1f1c51d9ac1f568
2319802305 <2319802305@qq.com>
2019-04-19 17:58:04 +0800

    提交人：杨军超 提交信息：根据用户账号和订单状态ID查询所有订单 提交时间：2019-4-19 18:00

---
+++ src/main/java/com/aaa/project/system/keyInfo/domain/KeyInfo.java
@@ -0,0 +1,59 @@
+package com.aaa.project.system.keyInfo.domain;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.aaa.framework.web.domain.BaseEntity;
+
+/**
+ * 钥匙表 cw_key_info
+ * 
+ * @author aaa
+ * @date 2019-04-19
+ */
+public class KeyInfo extends BaseEntity
+{
+	private static final long serialVersionUID = 1L;
+	
+	/** 编号 */
+	private Integer id;
+	/** 钥匙柜编号 */
+	private Integer containerId;
+	/** 状态编号 */
+	private Integer statusId;
+
+	public void setId(Integer id) 
+	{
+		this.id = id;
+	}
+
+	public Integer getId() 
+	{
+		return id;
+	}
+	public void setContainerId(Integer containerId) 
+	{
+		this.containerId = containerId;
+	}
+
+	public Integer getContainerId() 
+	{
+		return containerId;
+	}
+	public void setStatusId(Integer statusId) 
+	{
+		this.statusId = statusId;
+	}
+
+	public Integer getStatusId() 
+	{
+		return statusId;
+	}
+
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("id", getId())
+            .append("containerId", getContainerId())
+            .append("statusId", getStatusId())
+            .toString();
+    }
+}
