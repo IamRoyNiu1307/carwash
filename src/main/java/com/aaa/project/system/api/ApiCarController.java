1346aa43dfbb8ce75b2729712f3f5f3869bf8ab9
文威 <749834002@qq.com>
2019-04-20 09:13:13 +0800

    提交人：查文威 提交信息：上传车辆信息更改 version 1.2 提交时间： 2019-04-20  9:12 上午

--- src/main/java/com/aaa/project/system/api/ApiCarController.java
+++ src/main/java/com/aaa/project/system/api/ApiCarController.java
@@ -1,16 +1,11 @@
 package com.aaa.project.system.api;
 
 import com.aaa.common.exception.file.FileNameLengthLimitExceededException;
-import com.aaa.common.utils.file.FileUploadUtils;
 import com.aaa.framework.web.domain.AjaxResult;
 import com.aaa.project.system.carImage.service.ICarImageService;
 import com.aaa.project.system.carInfo.domain.CarInfo;
 import org.apache.tomcat.util.http.fileupload.FileUploadBase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,9 +22,9 @@ public class ApiCarController {
     private ICarImageService carImageService;
 
     @RequestMapping(value = "/setCarInfo", method = RequestMethod.POST)
-    public AjaxResult setCarInfo(@RequestBody CarInfo carInfo){
+    public AjaxResult setCarInfo(@RequestBody CarInfo carInfo) {
         AjaxResult ajaxResult = new AjaxResult();
-        if(carInfo.getId() != null){
+        if (carInfo.getId() != null) {
             ajaxResult.put("carInfo", carInfo);
         }
         return ajaxResult;
@@ -45,8 +40,10 @@ public class ApiCarController {
     @RequestMapping("/setCarImages")
     public AjaxResult setCarImages(@RequestParam(value = "carInfoId") int carInfoId, @RequestParam(value = "file") MultipartFile file) throws FileUploadBase.FileSizeLimitExceededException, FileNameLengthLimitExceededException, IOException {
         AjaxResult ajaxResult = new AjaxResult();
+        //更新信息
         String result = carImageService.UpdateCarImageByCarInfoId(carInfoId, file);
-        ajaxResult.put("msg",result);
+        ajaxResult.put("code", 0);
+        ajaxResult.put("msg", result);
         return ajaxResult;
     }
 }
